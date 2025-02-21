12b8bd9a5a2d375c64d99ee1729b77733d5d2fc6
sandeep-varma-k <sandeepvarmak7@gmail.com>
2025-02-20 22:13:33 -0500

    vite config changes for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sandeep-portfolio/",
+  base: "/portfolio/",
   plugins: [react()],
 })
